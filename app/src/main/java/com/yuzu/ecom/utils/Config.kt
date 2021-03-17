38777c756688c044758549e6bda1458918187d6b
YUZUFX\user <yustar.shooter@gmail.com>
2021-03-17 11:38:02 +0700

    hide google request token

---
+++ app/src/main/java/com/yuzu/ecom/utils/Config.kt
@@ -0,0 +1,7 @@
+package com.yuzu.ecom.utils
+
+/**
+ * Created by Yustar Pramudana on 17/03/2021
+ */
+
+const val GOOGLE_REQUEST_ID_TOKEN = "835994216944-9itcrgoont0atm8s63a1cm6d81ghkjel.apps.googleusercontent.com"
